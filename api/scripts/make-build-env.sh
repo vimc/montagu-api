75e6b18c803c1185a3699f65325edb1ff4478e8d
Martin Eden <martin.s.eden@gmail.com>
2017-03-23 10:49:59 +0000

    Add missing part of build-arg command

--- api/scripts/make-build-env.sh
+++ api/scripts/make-build-env.sh
@@ -1,3 +1,3 @@
 set -e
 git_id=$(git rev-parse --short HEAD)
-docker build --tag montagu-api-build-environment --build-arg $git_id .
+docker build --tag montagu-api-build-environment --build-arg api_docker_version=$git_id .
