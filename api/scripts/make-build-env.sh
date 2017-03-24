39445f3a38f03fa86008e4f8fb35aff241e5bd52
Martin Eden <martin.s.eden@gmail.com>
2017-03-24 10:51:55 +0000

    Use openjdk/debian base image for build, and also tag with branch name

--- api/scripts/make-build-env.sh
+++ api/scripts/make-build-env.sh
@@ -1,3 +1,8 @@
 set -e
 git_id=$(git rev-parse --short HEAD)
-docker build --tag montagu-api-build-environment --build-arg api_docker_version=$git_id .
+git_branch=$(git symbolic-ref --short HEAD)
+
+docker build --tag montagu-api-build-environment \
+	--build-arg git_id=$git_id \
+	--build-arg git_branch=$git_branch \
+	.
