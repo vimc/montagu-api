91c5e12e8a9f010edcacaa4050643f0dd5d27f54
Martin Eden <martin.s.eden@gmail.com>
2017-04-20 15:31:13 +0100

    Make sure error messages are quoted.

--- src/testHelpers/src/main/kotlin/org/vaccineimpact/api/test_helpers/TeamCityIntegration.kt
+++ src/testHelpers/src/main/kotlin/org/vaccineimpact/api/test_helpers/TeamCityIntegration.kt
@@ -1,8 +1,5 @@
 package org.vaccineimpact.api.test_helpers
 
-import org.junit.rules.TestWatcher
-import org.junit.runner.Description
-
 class TeamCityIntegration : org.junit.rules.TestWatcher()
 {
     override fun starting(description: org.junit.runner.Description)
@@ -17,7 +14,7 @@ class TeamCityIntegration : org.junit.rules.TestWatcher()
 
     override fun failed(e: Throwable, description: org.junit.runner.Description)
     {
-        println("##teamcity[testFailed name='${description.name()}' message=${e.message} details=$e]")
+        println("##teamcity[testFailed name='${description.name()}' message='${e.message}' details='$e']")
     }
 
     private fun org.junit.runner.Description.name() = "${this.className}.${this.methodName}"
