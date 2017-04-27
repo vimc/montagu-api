57471c5530973f936eeb82c571b2b0cdf4228757
Martin Eden <martin.s.eden@gmail.com>
2017-04-27 10:18:24 +0100

    Move blackbox tests outside of docker-compose

--- scripts/run-blackbox-tests.ps1
+++ scripts/run-blackbox-tests.ps1
@@ -1,4 +1,4 @@
-export MONTAGU_API_VERSION=$(git rev-parse --short HEAD)
+$env:MONTAGU_API_VERSION = git rev-parse --short HEAD
 docker-compose --project-name montagu up -d
 docker build -f Dockerfile-blackbox -t montagu-api-blackbox-tests .
 docker run --network montagu_default montagu-api-blackbox-tests
