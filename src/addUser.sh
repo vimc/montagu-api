bc44081cda0cfae9cf54cf0d35d464f397bc2b67
Martin Eden <martin.s.eden@gmail.com>
2017-05-05 11:40:28 +0100

    Rename addUser to userCLI

--- src/addUser.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-./gradlew :addUser:installDist
-./addUser/build/install/addUser/bin/addUser
---
+++ src/user.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+./gradlew :userCLI:installDist
+./userCLI/build/install/userCLI/bin/userCLI
