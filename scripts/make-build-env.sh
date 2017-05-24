0f59cdb8b737b072689d0618176ae7d3d532e159
Martin Eden <martin.s.eden@gmail.com>
2017-05-24 10:45:19 +0100

    Add continuous integration task that builds the CLI and dockerises it

--- scripts/make-build-env.sh
+++ scripts/make-build-env.sh
@@ -1,8 +1,1 @@
-set -e
-git_id=$(git rev-parse --short HEAD)
-git_branch=$(git symbolic-ref --short HEAD)
-
-docker build --tag montagu-api-build-environment \
-	--build-arg git_id=$git_id \
-	--build-arg git_branch=$git_branch \
-	.
+docker build --tag montagu-api-build-environment .
