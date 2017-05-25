20148dffe24c0269ac482d57ad0ce7c96522cfc4
Martin Eden <martin.s.eden@gmail.com>
2017-05-25 11:38:09 +0100

    Make Blackbox tests always pull latest version of database and API

--- scripts/run-blackbox-tests.sh
+++ scripts/run-blackbox-tests.sh
@@ -1,5 +1,9 @@
 export MONTAGU_API_VERSION=$(git rev-parse --short HEAD)
+
+docker-compose pull
 docker-compose --project-name montagu up -d
+
 docker build -f Dockerfile-blackbox -t montagu-api-blackbox-tests .
 docker run --network montagu_default montagu-api-blackbox-tests
+
 docker-compose down
