c2787e1cf2e972a0b455f0f2a61d15b874ad9224
Martin Eden <martin.s.eden@gmail.com>
2017-06-07 14:55:33 +0100

    Remove certificate tool from API (now it has its own repo)

--- scripts/run-blackbox-tests.sh
+++ scripts/run-blackbox-tests.sh
@@ -1,8 +1,9 @@
 export MONTAGU_API_VERSION=$(git rev-parse --short HEAD)
+cert_tool_version=d3f4ecb
 
 docker-compose pull
 docker-compose --project-name montagu up -d
-./scripts/generate-self-signed-certificate.sh password
+docker run montagu.dide.ic.ac.uk:5000/montagu-cert-tool:$cert_tool_version gen-self-signed password > keystore
 ./scripts/add-certificate-to-docker.sh keystore montagu_api_1
 
 docker build -f blackbox.Dockerfile -t montagu-api-blackbox-tests .
