165bfbea378ce61e91c51075db4538a0d9def022
Martin Eden <martin.s.eden@gmail.com>
2017-06-08 11:20:46 +0100

    Fix script to use renamed project

--- src/runAPIForWebappDev.sh
+++ src/runAPIForWebappDev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ./gradlew :stopDatabase
 
-./gradlew :startDatabase :blackboxTests:run \
+./gradlew :startDatabase :generateTestData \
 && ./user.sh add "Test User" test.user test@example.com password \
 && ./user.sh addRole test.user user \
 && ./user.sh addRole test.user member modelling-group IC-Garske \
