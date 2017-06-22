e6acece961e84206b05dd04a43b141176394723c
Alex Hill <alex.hill@gmail.com>
2017-06-22 14:33:40 +0100

    new UserController tests to test  permissions

--- src/app/src/test/kotlin/org/vaccineimpact/api/tests/controllers/UserControllerTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/api/tests/controllers/UserControllerTests.kt
@@ -9,6 +9,7 @@ import org.vaccineimpact.api.app.controllers.UserController
 import org.vaccineimpact.api.app.errors.UnknownObjectError
 import org.vaccineimpact.api.app.repositories.UserRepository
 import org.vaccineimpact.api.models.*
+import org.vaccineimpact.api.models.permissions.ReifiedPermission
 import org.vaccineimpact.api.models.permissions.RoleAssignment
 
 class UserControllerTests : ControllerTests<UserController>()
@@ -17,16 +18,36 @@ class UserControllerTests : ControllerTests<UserController>()
             = UserController(controllerContext)
 
     @Test
-    fun `getUserByUsername returns user`()
+    fun `getUserByUsername returns user without roles`()
     {
         val userName = "test"
-        val user = User("test", "test name", "test@test.com", null, listOf(RoleAssignment("member", "", "")))
+        val user = User("test", "test name", "test@test.com", null)
 
         val controllerContext = mockControllerContext(mock<UserRepository> {
             on { this.getUserByUsername(userName) } doReturn user
         })
         val context = mock<ActionContext> {
-            on { hasPermission(any()) } doReturn true
+            on { hasPermission(ReifiedPermission("users.read", Scope.Global())) } doReturn true
+            on { hasPermission(ReifiedPermission("roles.read", Scope.Global())) } doReturn false
+            on { params(":username") } doReturn userName
+        }
+
+        val controller = UserController(controllerContext)
+        assertThat(controller.getUser(context)).isEqualToComparingFieldByField(user)
+    }
+
+    @Test
+    fun `getUserByUsername returns user with roles`()
+    {
+        val userName = "test"
+        val user = UserWithRoles("test", "test name", "test@test.com", null, listOf(RoleAssignment("member", "", "")))
+
+        val controllerContext = mockControllerContext(mock<UserRepository> {
+            on { this.getUserByUsernameWithRoles(userName) } doReturn user
+        })
+        val context = mock<ActionContext> {
+            on { hasPermission(ReifiedPermission("users.read", Scope.Global())) } doReturn true
+            on { hasPermission(ReifiedPermission("roles.read", Scope.Global())) } doReturn true
             on { params(":username") } doReturn userName
         }
 
