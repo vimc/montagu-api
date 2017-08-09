e73fa88fefa70feafbcf9b0e3c94c53d23b67c9b
Alex Hill <alex.hill@gmail.com>
2017-08-09 19:41:11 +0100

    remove varianst from query

--- src/app/src/main/kotlin/org/vaccineimpact/api/app/repositories/jooq/JooqTouchstoneRepository.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/api/app/repositories/jooq/JooqTouchstoneRepository.kt
@@ -117,12 +117,11 @@ class JooqTouchstoneRepository(
 
 
     private fun getDemographicStatisticTypesQuery(touchstoneId: String):
-            SelectOnConditionStep<Record7<Int, String, String, Boolean, String, String, String>>
+            SelectOnConditionStep<Record6<Int, String, String, Boolean, String, String>>
     {
         val statsInTouchstoneCountriesAndSources =
                 dsl.selectDistinct(DEMOGRAPHIC_SOURCE.NAME.`as`("sourceName"),
                         DEMOGRAPHIC_STATISTIC.DEMOGRAPHIC_STATISTIC_TYPE.`as`("typeId"),
-                        DEMOGRAPHIC_STATISTIC.DEMOGRAPHIC_VARIANT.`as`("variantId"),
                         TOUCHSTONE_COUNTRY.COUNTRY.`as`("country"))
                         .from(DEMOGRAPHIC_STATISTIC)
                         .join(TOUCHSTONE_COUNTRY)
@@ -141,14 +140,11 @@ class JooqTouchstoneRepository(
                         DEMOGRAPHIC_STATISTIC_TYPE.CODE,
                         DEMOGRAPHIC_STATISTIC_TYPE.NAME,
                         DEMOGRAPHIC_STATISTIC_TYPE.GENDER_IS_APPLICABLE,
-                        DEMOGRAPHIC_VARIANT.NAME,
                         field(name("s", "sourceName"), String::class.java),
                         field(name("s", "country"), String::class.java))
                 .from(table(name("s")))
                 .join(DEMOGRAPHIC_STATISTIC_TYPE)
                 .on(DEMOGRAPHIC_STATISTIC_TYPE.ID.eq(field(name("s", "typeId"), Int::class.java)))
-                .join(DEMOGRAPHIC_VARIANT)
-                .on(DEMOGRAPHIC_VARIANT.ID.eq(field(name("s", "variantId"), Int::class.java)))
     }
 
     private fun getScenariosFromRecords(records: Result<Record>): List<Scenario>
