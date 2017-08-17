89a0a7ab9b27332c25662acd695af1dbd8e3d8d4
Martin Eden <martin.s.eden@gmail.com>
2017-08-17 14:41:39 +0100

    Add unique codes to unexpected errors.

--- src/app/src/main/kotlin/org/vaccineimpact/api/app/errors/UnexpectedError.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/api/app/errors/UnexpectedError.kt
@@ -1,7 +1,21 @@
 package org.vaccineimpact.api.app.errors
 
+import org.apache.commons.lang3.RandomStringUtils
+import org.vaccineimpact.api.db.Config
 import org.vaccineimpact.api.models.ErrorInfo
 
-class UnexpectedError : MontaguError(500, listOf(
-        ErrorInfo("unexpected-error", "An unexpected error occurred. Please see server logs for more details")
-))
+val supportAddress = Config["contact.support"]
+
+// The purpose of the code, even though it's not used anywhere else, is that the full text of the error gets logged.
+// So if a user gives you a code like UAA-BBB-CCC you can then search through the logs to find and see the associated
+// stack trace.
+class UnexpectedError : MontaguError(500, listOf(ErrorInfo(
+        "unexpected-error",
+        "An unexpected error occurred. Please contact support at $supportAddress and quote this code: ${newCode()}"
+)))
+
+private fun newCode(): String
+{
+    fun r(count: Int) = RandomStringUtils.randomAlphabetic(count).toLowerCase()
+    return "u${r(2)}-${r(3)}-${r(3)}"
+}
