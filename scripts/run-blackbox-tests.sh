1479fbab63b3af5a72ee201f5ed3f538b7c480f8
Martin Eden <martin.s.eden@gmail.com>
2017-08-31 14:21:21 +0100

    Reorder so that tests get pushed before being run, to allow faster reuse

--- scripts/run-blackbox-tests.sh
+++ scripts/run-blackbox-tests.sh
@@ -11,18 +11,11 @@ docker-compose --project-name montagu up -d
 docker exec montagu_api_1 mkdir -p /etc/montagu/api/
 docker exec montagu_api_1 touch /etc/montagu/api/go_signal
 
-# Build and run image that can run blackbox tests
+# Build and image that can run blackbox tests
 docker build --tag libsodium -f libsodium.Dockerfile .
 docker build -f blackbox.Dockerfile -t montagu-api-blackbox-tests .
-docker run \
-  --network montagu_default \
-  -v montagu_emails:/tmp/montagu_emails \
-  montagu-api-blackbox-tests
 
-# Tear down
-docker-compose --project-name montagu down
-
-# Push blackbox tests so they can be reused
+# Push blackbox tests image so it can be reused
 registry=docker.montagu.dide.ic.ac.uk:5000
 name=montagu-api-blackbox-tests
 docker_tag=$registry/$name
@@ -33,4 +26,14 @@ docker tag montagu-api-blackbox-tests $commit_tag
 docker tag montagu-api-blackbox-tests $branch_tag
 docker push $commit_tag 
 docker push $branch_tag
+
+# Run the tests
+docker run \
+  --network montagu_default \
+  -v montagu_emails:/tmp/montagu_emails \
+  montagu-api-blackbox-tests
+
+# Tear down
+docker-compose --project-name montagu down
+
 
