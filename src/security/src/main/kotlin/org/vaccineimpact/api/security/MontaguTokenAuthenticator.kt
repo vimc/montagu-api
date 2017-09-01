b7c9b9c49c5cff3e5dd715845b02b930ba003466
Martin Eden <martin.s.eden@gmail.com>
2017-09-01 16:39:04 +0100

    Remove debug text.

--- src/security/src/main/kotlin/org/vaccineimpact/api/security/MontaguTokenAuthenticator.kt
+++ src/security/src/main/kotlin/org/vaccineimpact/api/security/MontaguTokenAuthenticator.kt
@@ -1,7 +1,6 @@
 package org.vaccineimpact.api.security
 
 import com.nimbusds.jwt.JWT
-import org.pac4j.core.context.WebContext
 import org.pac4j.core.credentials.TokenCredentials
 import org.pac4j.core.exception.CredentialsException
 import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator
@@ -9,12 +8,6 @@ import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator
 class MontaguTokenAuthenticator(private val tokenHelper: WebTokenHelper)
     : JwtAuthenticator(tokenHelper.signatureConfiguration)
 {
-    override fun validate(credentials: TokenCredentials, context: WebContext?)
-    {
-        logger.info("TOKEN " + credentials.token)
-        super.validate(credentials, context)
-    }
-
     override fun createJwtProfile(credentials: TokenCredentials, jwt: JWT)
     {
         super.createJwtProfile(credentials, jwt)
