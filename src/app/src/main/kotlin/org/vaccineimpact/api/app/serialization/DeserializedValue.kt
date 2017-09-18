bba19497ae5481843d82e07f60a25f518a5d898a
Martin Eden <martin.s.eden@gmail.com>
2017-09-18 10:20:45 +0100

    Remove unused class.

--- src/app/src/main/kotlin/org/vaccineimpact/api/app/serialization/DeserializedValue.kt
+++
@@ -1,9 +0,0 @@
-package org.vaccineimpact.api.app.serialization
-
-import org.vaccineimpact.api.models.ErrorInfo
-
-sealed class DeserializedValue
-{
-    class Value(val value: Any?) : DeserializedValue()
-    class Error(val errorInfo: ErrorInfo): DeserializedValue()
-}
