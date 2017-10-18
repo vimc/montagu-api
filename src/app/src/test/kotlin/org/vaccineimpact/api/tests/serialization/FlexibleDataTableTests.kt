1f4c92a09bb201ae510c84affbced9f6b47149cb
Alex Hill <alex.hill@gmail.com>
2017-10-18 11:21:40 +0100

    remove FlexibleData type

--- src/app/src/test/kotlin/org/vaccineimpact/api/tests/serialization/FlexibleDataTableTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/api/tests/serialization/FlexibleDataTableTests.kt
@@ -10,20 +10,18 @@ import org.vaccineimpact.api.test_helpers.MontaguTests
 class FlexibleDataTableTests : MontaguTests()
 {
     data class ABC(val a: String, val b: String, val c: Map<String, String>)
-    data class FlexibleABC(override val rows: Iterable<ABC>, override val flexibleHeaders: Iterable<String>)
-        : FlexibleData<ABC>
 
     @Test
     fun `headers are written in order of constructor`()
     {
-        val table = FlexibleDataTable.new<ABC>(FlexibleABC(listOf(), listOf()))
+        val table = FlexibleDataTable.new<ABC>(listOf(), listOf())
         Assertions.assertThat(serialize(table)).isEqualTo(""""a","b"""")
     }
 
     @Test
     fun `extra headers are written at the end`()
     {
-        val table = FlexibleDataTable.new<ABC>(FlexibleABC(listOf(), listOf("extra1", "extra2")))
+        val table = FlexibleDataTable.new<ABC>(listOf(), listOf("extra1", "extra2"))
         Assertions.assertThat(serialize(table)).isEqualTo(""""a","b","extra1","extra2"""")
     }
 
@@ -34,7 +32,7 @@ class FlexibleDataTableTests : MontaguTests()
                 ABC("g", "h", mapOf("extra1" to "i", "extra2" to "j")),
                 ABC("x", "y", mapOf("extra1" to "z", "extra2" to "w"))
         )
-        val table = FlexibleDataTable.new<ABC>(FlexibleABC(data, listOf("extra1", "extra2")))
+        val table = FlexibleDataTable.new<ABC>(data, listOf("extra1", "extra2"))
 
         Assertions.assertThat(serialize(table)).isEqualTo(""""a","b","extra1","extra2"
 "g","h","i","j"
