148dcbea21d74bd1f36d7d617835e032a76698ba
Martin Eden <martin.s.eden@gmail.com>
2017-12-06 12:23:21 +0000

    Use db submodule scripts to start and stop database

--- scripts/start-database.sh
+++ scripts/start-database.sh
@@ -1,38 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-# Expects the following environment variables:
-# DB_VERSION (the tag of the database + migrations image to use)
-# DB_PORT    (the port that the db container should expose to the host machine)
-# NAME       (the name to assign to the db container)
-registry=docker.montagu.dide.ic.ac.uk:5000
-db_image=$registry/montagu-db:$DB_VERSION
-migrations_image=$registry/montagu-migrate:$DB_VERSION
-url=jdbc:postgresql://localhost:$DB_PORT/montagu
-
-echo "Using these images:"
-echo " - $db_image"
-echo " - $migrations_image"
-docker pull $db_image
-docker pull $migrations_image
+here=$(dirname $0)
 
 # If the database is already running, stop it
-if docker top $NAME &>/dev/null; then
+if docker top db &>/dev/null; then
     echo "Stopping database"
-    docker stop $NAME
+    $here/../db/scripts/stop.sh
     sleep 1s
 fi
 
 echo "Starting database"
-docker run -d --rm -p "$DB_PORT:5432" --name $NAME \
-    --entrypoint start-with-config.sh \
-    $db_image /postgresql.test.conf
-
-docker exec $NAME montagu-wait.sh
-
-echo "Migrating"
-docker run --rm --network=host $migrations_image migrate -url=$url
+$here/../db/scripts/start.sh $DB_VERSION
 
 echo "----------------------------------------"
-echo "Database is now running on port $DB_PORT"
+echo "Database is now running on port 5432"
 
---
+++ scripts/stop-database.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+here=$(dirname $0)
+$here/../db/scripts/stop.sh
