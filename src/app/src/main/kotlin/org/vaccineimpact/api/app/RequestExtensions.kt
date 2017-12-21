9d0c76df3b526329fa56868343c62041c9d022dd
Martin Eden <martin.s.eden@gmail.com>
2017-12-21 16:03:36 +0000

    Add explanatory comment.

--- src/app/src/main/kotlin/org/vaccineimpact/api/app/RequestExtensions.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/api/app/RequestExtensions.kt
@@ -7,6 +7,9 @@ import spark.Request
 fun Request.consumeRemainder()
 {
     val inputStream = this.raw()?.inputStream
+
+    // This null check is just here to make unit testing easier - this way
+    // we don't have to mock out .raw in so many tests
     if (inputStream != null)
     {
         val buffer = ByteArray(8096)
