7166d32491ee83e3d7958dea48171a4cb47715a2
Alex Hill <alex.hill@gmail.com>
2018-01-12 17:44:14 +0000

    add ACAO

--- src/app/src/main/kotlin/org/vaccineimpact/api/app/controllers/AuthenticationController.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/api/app/controllers/AuthenticationController.kt
@@ -36,7 +36,8 @@ class AuthenticationController(context: ActionContext,
                 val user = context.userProfile!!.montaguUser()!!
                 val token = tokenHelper.generateToken(user)
                 val shinyToken = tokenHelper.generateToken(user)
-                context.addResponseHeader("Set-Cookie", "jwt_token=test")
+                context.addResponseHeader("Set-Cookie", "jwt_token=test; Path=/")
+                context.addResponseHeader("Access-Control-Allow-Origin", "http://localhost:5000/")
                 userRepository.updateLastLoggedIn(user.username)
                 return SuccessfulAuthentication(shinyToken, token, tokenHelper.lifeSpan)
             }
