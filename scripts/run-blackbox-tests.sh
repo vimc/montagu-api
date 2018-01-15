0a209ae75bcf04d9b61de034bdf0af3c359ebc18
Alex Hill <alex.hill@gmail.com>
2018-01-15 17:07:56 +0000

    try waiting for databases

--- scripts/run-blackbox-tests.sh
+++ scripts/run-blackbox-tests.sh
@@ -13,6 +13,9 @@ docker-compose --project-name montagu up -d
 docker exec montagu_api_1 mkdir -p /etc/montagu/api/
 docker exec montagu_api_1 touch /etc/montagu/api/go_signal
 
+docker exec --network=montagu_default db montagu-wait.sh
+docker exec --network=montagu_default db_annex montagu-wait.sh
+
 docker pull $migrate_image
 docker run --rm --network=montagu_default $migrate_image
 docker run --rm --network=montagu_default $migrate_image -configFile=conf/flyway-annex.conf migrate
