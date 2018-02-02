e1906dffdb785098949d8e6fde1d5a592c2dd9bb
Martin Eden <martin.s.eden@gmail.com>
2018-02-02 12:12:10 +0000

    Add user mapping

--- scripts/run-blackbox-tests.sh
+++ scripts/run-blackbox-tests.sh
@@ -13,12 +13,18 @@ docker-compose --project-name montagu up -d
 docker exec montagu_api_1 mkdir -p /etc/montagu/api/
 docker exec montagu_api_1 touch /etc/montagu/api/go_signal
 
+# This chunk should be refactored to be shared with the db repo
+# -------------------------------------------------------------
 docker exec montagu_db_1 montagu-wait.sh
 docker exec montagu_db_annex_1 montagu-wait.sh
 
 docker pull $migrate_image
 docker run --rm --network=montagu_default $migrate_image
 docker run --rm --network=montagu_default $migrate_image -configFile=conf/flyway-annex.conf migrate
+
+docker exec montagu_db_1 psql -U vimc -d montagu -c \
+       "CREATE USER MAPPING FOR vimc SERVER db_annex OPTIONS (user 'vimc', password 'changeme');"
+# -------------------------------------------------------------
 
 # Build and image that can run blackbox tests
 docker build --tag libsodium -f libsodium.Dockerfile .
