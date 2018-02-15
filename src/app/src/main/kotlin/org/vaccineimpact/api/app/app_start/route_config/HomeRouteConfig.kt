87127b08c57fc46f1d5f1598836fce44f44fcd5c
Martin Eden <martin.s.eden@gmail.com>
2018-02-15 12:29:43 +0000

    New failing tests

--- src/app/src/main/kotlin/org/vaccineimpact/api/app/app_start/route_config/HomeRouteConfig.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/api/app/app_start/route_config/HomeRouteConfig.kt
@@ -2,12 +2,16 @@ package org.vaccineimpact.api.app.app_start.route_config
 
 import org.vaccineimpact.api.app.app_start.Endpoint
 import org.vaccineimpact.api.app.app_start.json
+import org.vaccineimpact.api.app.app_start.secure
 import org.vaccineimpact.api.app.controllers.HomeController
 
 object HomeRouteConfig : RouteConfig
 {
     override val endpoints = listOf(
             Endpoint("/", HomeController::class, "index")
-                    .json()
+                    .json(),
+
+            Endpoint("/meta/simulate-error/", HomeController::class, "simulateError")
+                    .secure()
     )
 }
--- src/blackboxTests/src/test/kotlin/org/vaccineimpact/api/blackboxTests/tests/AccessLogTests.kt
+++ src/blackboxTests/src/test/kotlin/org/vaccineimpact/api/blackboxTests/tests/AccessLogTests.kt
@@ -1,6 +1,8 @@
 package org.vaccineimpact.api.blackboxTests.tests
 
+import com.beust.klaxon.json
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
 import org.bouncycastle.util.IPAddress
 import org.junit.Test
 import org.vaccineimpact.api.blackboxTests.helpers.RequestHelper
@@ -11,6 +13,7 @@ import org.vaccineimpact.api.db.fieldsAsList
 import org.vaccineimpact.api.models.permissions.PermissionSet
 import org.vaccineimpact.api.security.UserHelper
 import org.vaccineimpact.api.test_helpers.DatabaseTest
+import org.vaccineimpact.api.validateSchema.JSONValidator
 import java.beans.ConstructorProperties
 import java.sql.Timestamp
 import java.time.Instant
@@ -65,26 +68,67 @@ class AccessLogTests : DatabaseTest()
         }
     }
 
+    @Test
+    fun `logs request which results in 500 unexpected error`()
+    {
+        val token = TestUserHelper.setupTestUserAndGetToken(PermissionSet("*/can-login"))
+        val helper = RequestHelper()
+        doThisAndCheck("test.user", "/meta/simulate-error/", 200) {
+            helper.get("/meta/simulate-error/", token)
+        }
+    }
+
+    @Test
+    fun `logs request which results in 400 error`()
+    {
+        val token = TestUserHelper.setupTestUserAndGetToken(PermissionSet("*/can-login", "*/users.create"))
+        val helper = RequestHelper()
+        doThisAndCheck("test.user", "/users/", 400) {
+            helper.post("/users/", json { obj("bad" to "worse") }, token)
+        }
+    }
+
+    @Test
+    fun `logs request which results in 404 error`()
+    {
+        val token = TestUserHelper.setupTestUserAndGetToken(PermissionSet("*/can-login"))
+        val helper = RequestHelper()
+        doThisAndCheck("test.user", "/not-a-real-URL/", 404) {
+            helper.get("/not-a-real-URL/", token)
+        }
+    }
+
     private fun doThisAndCheck(who: String?, what: String, result: Int?, action: () -> Unit)
     {
         val before = Instant.now()
+        JooqContext().use { db ->
+            db.dsl.deleteFrom(API_ACCESS_LOG).execute()
+        }
+
         action()
         val entry = JooqContext().use { db ->
             db.dsl.select(API_ACCESS_LOG.fieldsAsList())
                     .from(API_ACCESS_LOG)
                     .orderBy(API_ACCESS_LOG.TIMESTAMP.desc())
                     .fetchInto(Entry::class.java)
-                    .first()
+                    .firstOrNull()
         }
-        assertThat(entry.who).`as`("Who").isEqualTo(who)
+        if (entry != null)
+        {
+            assertThat(entry.who).`as`("Who").isEqualTo(who)
 
-        val timestamp = entry.timestamp.toInstant()
-        assertThat(timestamp).isAfterOrEqualTo(before)
-        assertThat(timestamp).isBeforeOrEqualTo(Instant.now())
+            val timestamp = entry.timestamp.toInstant()
+            assertThat(timestamp).isAfterOrEqualTo(before)
+            assertThat(timestamp).isBeforeOrEqualTo(Instant.now())
 
-        assertThat(entry.what).`as`("What").isEqualTo("/v1" + what)
-        assertThat(entry.result).`as`("Result").isEqualTo(result)
-        assertThat(IPAddress.isValid(entry.ipAddress)).`as`("IP Address is valid").isTrue()
+            assertThat(entry.what).`as`("What").isEqualTo("/v1" + what)
+            assertThat(entry.result).`as`("Result").isEqualTo(result)
+            assertThat(IPAddress.isValid(entry.ipAddress)).`as`("IP Address is valid").isTrue()
+        }
+        else
+        {
+            fail("Nothing was logged")
+        }
     }
 
     data class Entry
