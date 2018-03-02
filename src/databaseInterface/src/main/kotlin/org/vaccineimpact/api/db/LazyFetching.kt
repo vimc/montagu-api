e3f0310be552d21810baec15688a4c438e863f08
Martin Eden <martin.s.eden@gmail.com>
2018-03-02 14:15:40 +0000

    Use fetchNext instead of fetchOne

--- src/databaseInterface/src/main/kotlin/org/vaccineimpact/api/db/LazyFetching.kt
+++ src/databaseInterface/src/main/kotlin/org/vaccineimpact/api/db/LazyFetching.kt
@@ -7,13 +7,13 @@ import org.jooq.ResultQuery
 fun <TRecord : Record> ResultQuery<TRecord>.fetchSequence(): Sequence<TRecord>
 {
     val cursor = getCursor()
-    return generateSequence { cursor.fetchOne() }
+    return generateSequence { cursor.fetchNext() }
 }
 
 inline fun <TRecord : Record, reified TModel : Any> ResultQuery<TRecord>.fetchSequenceInto(): Sequence<TModel>
 {
     val cursor = getCursor()
-    return generateSequence { cursor.fetchOne().into(TModel::class.java) }
+    return generateSequence { cursor.fetchNext().into(TModel::class.java) }
 }
 
 fun <TRecord : Record> ResultQuery<TRecord>.getCursor(): Cursor<TRecord> = this.fetchSize(100).fetchLazy()
