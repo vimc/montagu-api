249b0eb6c6dd70eee7a694000b73fe80872c1449
Martin Eden <martin.s.eden@pm.me>
2018-06-08 15:26:27 +0100

    Add command line option to API to instead sign and output a token

---
+++ src/app/src/main/kotlin/org/vaccineimpact/api/app/app_start/Main.kt
@@ -0,0 +1,47 @@
+package org.vaccineimpact.api.app.app_start
+
+import org.docopt.Docopt
+import org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY
+import org.vaccineimpact.api.app.repositories.RepositoryFactory
+import java.io.File
+
+const val doc = """
+Montagu API
+
+Usage:
+    app [run]
+    app generate-token <permission>...
+"""
+
+fun main(args: Array<String>)
+{
+    val options = Docopt(doc).parse(args.toList())
+    if (options["generate-token"] as Boolean)
+    {
+        // Bit of a hack to disable logging messing up our neat output
+        System.setProperty(DEFAULT_LOG_LEVEL_KEY, "WARN")
+        @Suppress("UNCHECKED_CAST")
+        println(RootTokenGenerator().generate(options["<permission>"] as List<String>))
+    }
+    else
+    {
+        waitForGoSignal()
+        val api = MontaguApi()
+        api.run(RepositoryFactory())
+    }
+}
+
+// This is so that we can copy files into the Docker container after it exists
+// but before the API starts running.
+private fun waitForGoSignal()
+{
+    val path = File("/etc/montagu/api/go_signal")
+    println("Waiting for signal file at $path.")
+    println("(In development environments, run `sudo touch $path`)")
+
+    while (!path.exists())
+    {
+        Thread.sleep(2000)
+    }
+    println("Go signal detected. Running API")
+}
---
+++ src/app/src/main/kotlin/org/vaccineimpact/api/app/app_start/RootTokenGenerator.kt
@@ -0,0 +1,26 @@
+package org.vaccineimpact.api.app.app_start
+
+import org.vaccineimpact.api.models.permissions.ReifiedPermission
+import org.vaccineimpact.api.security.InternalUser
+import org.vaccineimpact.api.security.KeyHelper
+import org.vaccineimpact.api.security.UserProperties
+import org.vaccineimpact.api.security.WebTokenHelper
+import java.time.Duration
+
+class RootTokenGenerator
+{
+    fun generate(permissions: List<String>): String
+    {
+        val parsedPermissions = permissions.map { ReifiedPermission.parse(it) }
+        val dummyUserProperties = UserProperties(
+                "%dummy.user%",
+                "Fake user for generating tokens",
+                "montagu-help@imperial.ac.uk",
+                null,
+                null
+        )
+        val dummyUser = InternalUser(dummyUserProperties, emptyList(), parsedPermissions)
+        return WebTokenHelper(KeyHelper.keyPair)
+                .generateToken(dummyUser, Duration.ofDays(365))
+    }
+}
