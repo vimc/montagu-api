b0886354f38a99f1ce2aa92f1f8122edd1598c05
Martin Eden <martin.s.eden@pm.me>
2018-08-15 15:00:50 +0100

    Update blackbox tests

--- src/blackboxTests/src/test/kotlin/org/vaccineimpact/api/blackboxTests/tests/AuthenticationTests.kt
+++ src/blackboxTests/src/test/kotlin/org/vaccineimpact/api/blackboxTests/tests/AuthenticationTests.kt
@@ -6,22 +6,18 @@ import com.beust.klaxon.Parser
 import com.beust.klaxon.json
 import khttp.options
 import khttp.post
+import khttp.responses.Response
 import khttp.structures.authorization.BasicAuthorization
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.vaccineimpact.api.blackboxTests.helpers.EndpointBuilder
-import org.vaccineimpact.api.blackboxTests.helpers.RequestHelper
-import org.vaccineimpact.api.blackboxTests.helpers.TestUserHelper
-import org.vaccineimpact.api.blackboxTests.helpers.TokenFetcher
 import org.vaccineimpact.api.db.JooqContext
 import org.vaccineimpact.api.db.Tables.APP_USER
-import org.vaccineimpact.api.db.direct.addUserForTesting
-import org.vaccineimpact.api.db.direct.addUserWithRoles
-import org.vaccineimpact.api.models.Scope
-import org.vaccineimpact.api.models.permissions.ReifiedRole
-import org.vaccineimpact.api.security.*
+import org.vaccineimpact.api.security.UserHelper
 import org.vaccineimpact.api.test_helpers.DatabaseTest
+
+data class ResponseWithJsonBody(val response: Response, val body: JsonObject)
 
 class AuthenticationTests : DatabaseTest()
 {
@@ -38,63 +34,21 @@ class AuthenticationTests : DatabaseTest()
     fun `authentication fails without BasicAuth header`()
     {
         val result = post("email@example.com", "password", includeAuth = false)
-        assertDoesNotAuthenticate(result)
-    }
-
-    @Test
-    fun `can set shiny cookie`()
-    {
-        JooqContext().use {
-            it.addUserForTesting(TestUserHelper.username,
-                    email = TestUserHelper.email, password = TestUserHelper.defaultPassword)
-            it.ensureUserHasRole(TestUserHelper.username, ReifiedRole("reports-reviewer", Scope.Global()))
-            it.ensureUserHasRole(TestUserHelper.username, ReifiedRole("user", Scope.Global()))
-        }
-
-        val token = TokenFetcher().getToken(TestUserHelper.email, TestUserHelper.defaultPassword)
-                as TokenFetcher.TokenResponse.Token
-
-        val response = RequestHelper().get("/set-shiny-cookie/", token.token)
-
-        assertThat(response.statusCode).isEqualTo(200)
-
-        val cookie = response.headers["Set-Cookie"]!!
-        assertThat(cookie).contains("HttpOnly")
-        assertThat(cookie).contains("SameSite=Lax")
-
-        val shinyToken = cookie.substring(cookie.indexOf("=") + 1, cookie.indexOf(";"))
-        val claims = JWT.decode(shinyToken)
-        val allowedShiny = claims.getClaim("allowed_shiny")
-        assertThat(allowedShiny.asString()).isEqualTo("true")
-    }
-
-    @Test
-    fun `can clear shiny cookie`()
-    {
-        val response = RequestHelper().get("/clear-shiny-cookie/")
-
-        assertThat(response.statusCode).isEqualTo(200)
-
-        val cookieHeader = response.headers["Set-Cookie"]!!
-        assertThat(cookieHeader).contains("HttpOnly")
-        assertThat(cookieHeader).contains("SameSite=Lax")
-
-        val cookie = cookieHeader.substring(cookieHeader.indexOf("=") + 1, cookieHeader.indexOf(";"))
-        assertThat(cookie.isEmpty()).isTrue()
+        assertDoesNotAuthenticate(result.body)
     }
 
     @Test
     fun `unknown email does not authenticate`()
     {
         val result = post("bad@example.com", "password")
-        assertDoesNotAuthenticate(result)
+        assertDoesNotAuthenticate(result.body)
     }
 
     @Test
     fun `incorrect password does not authenticate`()
     {
         val result = post("email@example.com", "bad_password")
-        assertDoesNotAuthenticate(result)
+        assertDoesNotAuthenticate(result.body)
     }
 
     @Test
@@ -103,29 +57,38 @@ class AuthenticationTests : DatabaseTest()
         JooqContext().use {
             it.dsl.update(APP_USER).set(mapOf(APP_USER.PASSWORD_HASH to null)).execute()
         }
-        assertDoesNotAuthenticate(post("email@example.com", ""))
-        assertDoesNotAuthenticate(post("email@example.com", "password"))
+        assertDoesNotAuthenticate(post("email@example.com", "").body)
+        assertDoesNotAuthenticate(post("email@example.com", "password").body)
     }
 
     @Test
     fun `cannot authenticate with username`()
     {
         val result = post("user", "password")
-        assertDoesNotAuthenticate(result)
+        assertDoesNotAuthenticate(result.body)
     }
 
     @Test
     fun `correct password does authenticate`()
     {
-        val result = post("email@example.com", "password")
-        assertDoesAuthenticate(result)
+        val response = post("email@example.com", "password")
+        assertDoesAuthenticate(response.body)
+
+        val cookie = response.response.headers["Set-Cookie"]!!
+        assertThat(cookie).contains("HttpOnly")
+        assertThat(cookie).contains("SameSite=Strict")
+
+        val token = cookie.substring(cookie.indexOf("=") + 1, cookie.indexOf(";"))
+        val claims = JWT.decode(token)
+        val tokenType = claims.getClaim("token_type")
+        assertThat(tokenType.asString()).isEqualTo("bearer")
     }
 
     @Test
     fun `email authentication is not case sensitive`()
     {
         val result = post("EMAIL@example.cOm", "password")
-        assertDoesAuthenticate(result)
+        assertDoesAuthenticate(result.body)
     }
 
     @Test
@@ -169,14 +132,17 @@ class AuthenticationTests : DatabaseTest()
     {
         val url = EndpointBuilder.build("/authenticate/")
 
-        fun post(username: String, password: String, includeAuth: Boolean = true): JsonObject
+        fun post(username: String, password: String, includeAuth: Boolean = true): ResponseWithJsonBody
         {
             val auth = if (includeAuth) BasicAuthorization(username, password) else null
-            val text = post(url,
+            val response =  post(url,
                     data = mapOf("grant_type" to "client_credentials"),
-                    auth = auth).text
+                    auth = auth
+            )
+            val text = response.text
             println(text)
-            return Parser().parse(StringBuilder(text)) as JsonObject
+            val body = Parser().parse(StringBuilder(text)) as JsonObject
+            return ResponseWithJsonBody(response, body)
         }
     }
 }
