8f2d74e7dcd8d6e05ec504efb892f1bff2628b34
Martin Eden <martin.s.eden@pm.me>
2018-08-15 15:25:20 +0100

    Add comment.

--- src/security/src/main/kotlin/org/vaccineimpact/api/security/CookieName.kt
+++ src/security/src/main/kotlin/org/vaccineimpact/api/security/CookieName.kt
@@ -3,5 +3,6 @@ package org.vaccineimpact.api.security
 enum class CookieName(val cookieName: String)
 {
     Main("montagu_jwt_token"),
+    // This must have this name, as that's what Caddy expects
     Shiny("jwt_token")
 }
