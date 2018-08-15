e9fe59904ec887c2cbf3b4dbfd7cccefabc867b7
Martin Eden <martin.s.eden@pm.me>
2018-08-15 16:01:15 +0100

    Add TODO comment

--- src/app/src/main/kotlin/org/vaccineimpact/api/app/app_start/route_config/AuthenticationRouteConfig.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/api/app/app_start/route_config/AuthenticationRouteConfig.kt
@@ -20,6 +20,7 @@ object AuthenticationRouteConfig : RouteConfig
                     .json(),
             // This endpoint just removes cookies to log the user out so there is no reason to secure it
             // DEPRECATED: This is just an alias for logOut - to be removed once the webapps no longer use it
+            // TODO: VIMC-2084
             Endpoint("/clear-shiny-cookie/", controller, "logOut")
                     .json()
     )
