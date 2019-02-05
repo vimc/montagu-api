9b81f49c295d046400c2922a599532874c81b8f6
Alex Hill <alex.hill@gmail.com>
2019-02-05 13:36:46 +0000

    fix blackbox test

--- src/blackboxTests/src/test/kotlin/org/vaccineimpact/api/blackboxTests/tests/AuthenticationTests.kt
+++ src/blackboxTests/src/test/kotlin/org/vaccineimpact/api/blackboxTests/tests/AuthenticationTests.kt
@@ -66,7 +66,7 @@ class AuthenticationTests : DatabaseTest()
 
         val shinyToken = checkCookieAndGetValue(response, "jwt_token")
         val shinyClaims = JWT.decode(shinyToken)
-        val allowedShiny = shinyClaims.getClaim("allowed_shiny")
+        val allowedShiny = shinyClaims.getClaim("IC-Garske")
         assertThat(allowedShiny.asString()).isEqualTo("true")
     }
 
