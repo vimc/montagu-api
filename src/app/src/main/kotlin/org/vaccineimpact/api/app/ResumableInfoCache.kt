aba21e6e39f877dcfba0b3dda452fca4eaece4d9
Alex <alex.hill@gmail.com>
2019-02-19 15:58:25 +0000

    make cache recycle values

--- src/app/src/main/kotlin/org/vaccineimpact/api/app/ResumableInfoCache.kt
+++ src/app/src/main/kotlin/org/vaccineimpact/api/app/ResumableInfoCache.kt
@@ -2,6 +2,7 @@ package org.vaccineimpact.api.app
 
 import org.vaccineimpact.api.app.models.ResumableInfo
 import java.util.HashMap
+import java.util.concurrent.TimeUnit
 
 interface Cache<T> {
     operator fun get(uniqueIdentifier: String): T?
@@ -12,20 +13,32 @@ interface Cache<T> {
 object ResumableInfoCache: Cache<ResumableInfo>
 {
     private val memoryCache = HashMap<String, ResumableInfo>()
+    private var lastFlushTime = System.nanoTime()
+    private val flushInterval: Long = TimeUnit.HOURS.toMillis(1)
 
     override operator fun get(uniqueIdentifier: String): ResumableInfo?
     {
+        recycle()
         return memoryCache[uniqueIdentifier]
     }
 
     override fun put(item: ResumableInfo)
     {
+        recycle()
         memoryCache[item.uniqueIdentifier] = item
     }
 
     override fun remove(item: ResumableInfo)
     {
+        recycle()
         memoryCache.remove(item.uniqueIdentifier)
     }
 
+    private fun recycle() {
+        val shouldRecycle = System.nanoTime() - lastFlushTime >= TimeUnit.MILLISECONDS.toNanos(flushInterval)
+        if (shouldRecycle)
+        {
+            memoryCache.clear()
+        }
+    }
 }
