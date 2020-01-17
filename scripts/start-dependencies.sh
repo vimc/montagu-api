3ff9f5ed708108c3b942c4a15d53a109325f035a
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-01-17 10:41:24 +0000

    Revert "Use path relative to here in start-dependencies, should fix cli task on TeamCity"
    This reverts commit a3f59c5f09ff281438a97cccbffa593ade4f5d92.

--- scripts/start-dependencies.sh
+++ scripts/start-dependencies.sh
@@ -25,20 +25,18 @@ OW_MIGRATE_IMAGE="vimc/orderlyweb-migrate:master"
 ORDERLY_WEB_IMAGE="vimc/orderly-web:master"
 
 # create orderly db
-SRC=$(realpath $here/../src)
-echo "SRC ABS: $SRC"
-rm $SRC/demo -rf
-rm $SRC/git -rf
+rm $PWD/demo -rf
+rm $PWD/git -rf
 docker pull $ORDERLY_IMAGE
-docker run --rm --entrypoint create_orderly_demo.sh -v "$SRC:/orderly" -u $UID -w /orderly $ORDERLY_IMAGE .
+docker run --rm --entrypoint create_orderly_demo.sh -v "$PWD:/orderly" -u $UID -w /orderly $ORDERLY_IMAGE .
 
 # migrate to add orderlyweb tables
 docker pull $OW_MIGRATE_IMAGE
-docker run --rm -v "$SRC/demo:/orderly" $OW_MIGRATE_IMAGE
+docker run --rm -v "$PWD/demo:/orderly" $OW_MIGRATE_IMAGE
 
 # start orderlyweb
 docker pull $ORDERLY_WEB_IMAGE
-docker run -d -v "$SRC/demo:/orderly" -p 8888:8888 --net=host --name orderly-web $ORDERLY_WEB_IMAGE
+docker run -d -v "$PWD/demo:/orderly" -p 8888:8888 --net=host --name orderly-web $ORDERLY_WEB_IMAGE
 
 docker exec orderly-web mkdir -p /etc/orderly/web
 docker exec orderly-web touch /etc/orderly/web/go_signal
