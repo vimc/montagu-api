a934bfac91dfe9f2dccb5d3508adca55665f9dcc
Alex <alex.hill@gmail.com>
2020-02-11 11:24:25 +0000

    remove annex user mapping

--- scripts/run-blackbox-tests.sh
+++ scripts/run-blackbox-tests.sh
@@ -16,14 +16,11 @@ docker exec montagu_api_1 touch /etc/montagu/api/go_signal
 # This chunk should be refactored to be shared with the db repo
 # -------------------------------------------------------------
 docker exec montagu_db_1 montagu-wait.sh
-docker exec montagu_db_annex_1 montagu-wait.sh
 
 docker pull $migrate_image
 docker run --rm --network=montagu_default $migrate_image
 docker run --rm --network=montagu_default $migrate_image -configFile=conf/flyway-annex.conf migrate
 
-docker exec montagu_db_1 psql -U vimc -d montagu -c \
-       "CREATE USER MAPPING FOR vimc SERVER montagu_db_annex OPTIONS (user 'vimc', password 'changeme');"
 # -------------------------------------------------------------
 
 # Build and image that can run blackbox tests
