de8c9034bb06b14489d164e07af1a8e6689cafc6
Alex <alex.hill@gmail.com>
2020-02-12 18:23:13 +0000

    fix db command

--- scripts/run-blackbox-tests.sh
+++ scripts/run-blackbox-tests.sh
@@ -22,7 +22,7 @@ docker exec montagu_api_1 touch /etc/montagu/api/go_signal
 # -------------------------------------------------------------
 docker exec montagu_db_1 montagu-wait.sh
 
-docker exec db psql -U postgres -d montagu -c "create extension tablefunc;"
+docker exec montagu_db_1 psql -U postgres -d montagu -c "create extension tablefunc;"
 
 docker pull $migrate_image
 docker run --rm --network=montagu_default $migrate_image
