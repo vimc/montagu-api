afd4573818a13aa624ae811bc9f278ffff209182
Alex <alex.hill@gmail.com>
2020-10-14 11:51:13 +0100

    add gender to coverage ingestion row

--- src/app/src/test/kotlin/org/vaccineimpact/api/tests/controllers/coverage/PopulatingCoverageTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/api/tests/controllers/coverage/PopulatingCoverageTests.kt
@@ -8,9 +8,7 @@ import org.junit.Test
 import org.vaccineimpact.api.app.context.ActionContext
 import org.vaccineimpact.api.app.controllers.CoverageController
 import org.vaccineimpact.api.app.requests.PostDataHelper
-import org.vaccineimpact.api.models.ActivityType
-import org.vaccineimpact.api.models.CoverageIngestionRow
-import org.vaccineimpact.api.models.GAVISupportLevel
+import org.vaccineimpact.api.models.*
 import org.vaccineimpact.api.serialization.validation.ValidationException
 import org.vaccineimpact.api.test_helpers.MontaguTests
 
@@ -25,8 +23,8 @@ class PopulatingCoverageTests : MontaguTests()
         val sut = CoverageController(mockContext, mock(), PostDataHelper())
         val result = sut.getCoverageDataFromCSV()
         assertThat(result.toList()).containsExactly(
-                CoverageIngestionRow("HepB_BD", "AFG", ActivityType.CAMPAIGN, GAVISupportLevel.WITH, 2020, 1, 10, 100F, 78.8F),
-                CoverageIngestionRow("HepB_BD", "AFG", ActivityType.CAMPAIGN, GAVISupportLevel.WITH, 2021, 1, 10, 100F, 65.5F)
+                CoverageIngestionRow("HepB_BD", "AFG", ActivityType.CAMPAIGN, GAVISupportLevel.WITH, 2020, 1, 10, GenderEnum.BOTH, 100F, 78.8F),
+                CoverageIngestionRow("HepB_BD", "AFG", ActivityType.CAMPAIGN, GAVISupportLevel.WITH, 2021, 1, 10, GenderEnum.FEMALE, 100F, 65.5F)
         )
     }
 
@@ -67,24 +65,24 @@ class PopulatingCoverageTests : MontaguTests()
     }
 
     private val normalCSVDataString = """
-"vaccine", "country", "activity_type", "gavi_support", "year", "age_first", "age_last", "target", "coverage"
-   "HepB_BD",   "AFG",    "campaign",     "with",  "2020",         1,     10,    100, 78.8
-   "HepB_BD",   "AFG",    "campaign",     "with",  "2021",         1,      10,    100, 65.5
+"vaccine", "country", "activity_type", "gavi_support", "year", "age_first", "age_last", "gender", "target", "coverage"
+   "HepB_BD",   "AFG",    "campaign",     "with",  "2020",         1,     10,    "both", 100, 78.8
+   "HepB_BD",   "AFG",    "campaign",     "with",  "2021",         1,      10,    "Female", 100, 65.5
 """
 
     private val invalidSupportLevelCSVDataString = """
-"vaccine", "country", "activity_type", "gavi_support", "year", "age_first", "age_last", "target", "coverage"
-   "HepB_BD",   "AFG",    "campaign",     "withsupport",  "2020",         1,     10,    100, 78.8
+"vaccine", "country", "activity_type", "gavi_support", "year", "age_first", "age_last", "gender", "target", "coverage"
+   "HepB_BD",   "AFG",    "campaign",     "withsupport",  "2020",         1,     10,    "both", 100, 78.8
 """
 
     private val invalidActivityTypeCSVDataString = """
-"vaccine", "country", "activity_type", "gavi_support", "year", "age_first", "age_last", "target", "coverage"
-   "HepB_BD",   "AFG",    "a campaign",     "with",  "2020",         1,     10,    100, 78.8
+"vaccine", "country", "activity_type", "gavi_support", "year", "age_first", "age_last", "gender", "target", "coverage"
+   "HepB_BD",   "AFG",    "a campaign",     "with",  "2020",         1,     10,    100, "both", 78.8
 """
 
     private val invalidHeadersCSVDataString = """
-"vaccine", "country code", "activity", "gavi_support", "year", "age_first", "age_last", "target", "coverage"
-   "HepB_BD",   "AFG",    "a campaign",     "with",  "2020",         1,     10,    100, 78.8
+"vaccine", "country code", "activity", "gavi_support", "year", "age_first", "age_last", "gender", "target", "coverage"
+   "HepB_BD",   "AFG",    "a campaign",     "with",  "2020",         1,     10,    "both", 100, 78.8
 """
 
 }
