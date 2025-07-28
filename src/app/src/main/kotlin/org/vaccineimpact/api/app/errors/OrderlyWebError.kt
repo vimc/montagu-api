b475cc89bcfe9ed4adc2db4e71518745d6ffb885
Emma <EmmaRussellKilburn@gmail.com>
2025-07-28 17:14:50 +0100

    replace OrderlyWebError with PackitError

--- src/app/src/main/kotlin/org/vaccineimpact/api/app/errors/OrderlyWebError.kt
+++
@@ -1,7 +0,0 @@
-package org.vaccineimpact.api.app.errors
-
-import org.vaccineimpact.api.models.ErrorInfo
-
-class OrderlyWebError(message: String) : MontaguError(500, listOf(
-        ErrorInfo("orderly-web-error", message)
-))
